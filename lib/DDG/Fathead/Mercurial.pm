06e6f206fa765115b77ac2ee3516d1f9ee207282
Robert Picard <mail@robert.io>
2013-02-20 23:20:55 -0500

    Add meta perl file for Mercurial

---
+++ lib/DDG/Fathead/Mercurial.pm
@@ -0,0 +1,27 @@
+package DDG::Fathead::Mercurial;
+
+use DDG::Fathead;
+
+name "Mercurial Manual";
+
+description "Mercurial and hg information";
+
+source "Selenic Consulting";
+
+icon_url "/i/www.selenic.com.ico";
+
+topics "programming";
+
+category "reference";
+
+primary_example_queries
+    "hg summary",
+    "mercurial manifest";
+
+secondary_example_queries
+    "hg rollback",
+    "addremove",
+    "showconfig";
+
+1;
+
