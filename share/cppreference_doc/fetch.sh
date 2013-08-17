8c3becccb6e5e1402d132cbd07dffd292884ba1d
p12 <tir5c3@yahoo.co.uk>
2013-08-18 01:16:29 +0300

    CppreferenceDoc: update download location

--- share/cppreference_doc/fetch.sh
+++ share/cppreference_doc/fetch.sh
@@ -2,6 +2,6 @@
 mkdir -p download
 
 pushd download > /dev/null
-wget http://upload.cppreference.com/mwiki/images/a/a5/cppreference-doc-20130627.tar.gz
-tar -xzf cppreference-doc-20130627.tar.gz
+wget http://upload.cppreference.com/mwiki/images/e/e7/cppreference-doc-20130729-ddg.tar.gz
+tar -xzf cppreference-doc-20130729-ddg.tar.gz
 popd > /dev/null
